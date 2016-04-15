64fb73e1eecab3223a6f0bec19704c918bbaf401
JonahD <jonahd97@gmail.com>
2016-04-15 00:22:33 -0400

    Made website better

---
+++ main.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function() {
+	$("#about_mm").click(function(){
+		$("#about_content").slideToggle();
+		if($(this).html() == '<p id="about_mm_p">-</p>'){
+			$(this).html('<p id="about_mm_p">+</p>');
+		} else {
+			$(this).html('<p id="about_mm_p">-</p>');
+		}
+	});
+
+	$("#resume_mm").click(function(){
+		$("#resume_content").slideToggle();
+		if($(this).html() == '<p id="resume_mm_p">-</p>'){
+			$(this).html('<p id="resume_mm_p">+</p>');
+		} else {
+			$(this).html('<p id="resume_mm_p">-</p>');
+		}
+	});
+	
+	$("#projects_mm").click(function(){
+		$("#projects_content").slideToggle();
+		if($(this).html() == '<p id="projects_mm_p">-</p>'){
+			$(this).html('<p id="projects_mm_p">+</p>');
+		} else {
+			$(this).html('<p id="projects_mm_p">-</p>');
+		}
+	});
+
+	$("#contact_mm").click(function(){
+		$("#contact_content").slideToggle();
+		if($(this).html() == '<p id="contact_mm_p">-</p>'){
+			$(this).html('<p id="contact_mm_p">+</p>');
+		} else {
+			$(this).html('<p id="contact_mm_p">-</p>');
+		}
+	});
+});
